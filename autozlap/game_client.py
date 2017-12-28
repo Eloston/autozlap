802b3c27e17496b08c84c63c7b93a800388803a8
Eloston <eloston@programmer.net>
2017-12-28 19:38:37 +0000

    First idling bot without crashing
    This code is multiple months old. It will be refactored.

--- autozlap/game_client.py
+++ autozlap/game_client.py
@@ -25,28 +25,43 @@ import aiohttp
 from .networking import Connection
 
 class Vector:
-    """Represents a 2D vector"""
-    pass
+    """Represents a 2D vector in cartesian coordinates"""
+    def __init__(self, coordinates=None, vector_struct=None):
+        if coordinates:
+            self.x, self.y = coordinates #pylint: disable=invalid-name
+        else:
+            self.update(vector_struct)
 
-class Player:
-    """Represents a player"""
-    def __init__(self):
-        self.id = None
-        self.position = None
-        self.velocity = None
-        self.mace = None
+    def update(self, vector2d_struct):
+        """Update the vector values based on a vector2d Struct"""
+        self.x = vector2d_struct.x #pylint: disable=invalid-name
+        self.y = vector2d_struct.y #pylint: disable=invalid-name
 
 class Mace:
     """Represents a mace"""
-    def __init__(self):
-        self.position = None
-        self.velocity = None
-        self.player = None
+    def __init__(self, player, position, velocity):
+        self.player = player
+        self.position = position
+        self.velocity = velocity
+
+class Player:
+    """Represents a player"""
+    def __init__(self, id_num, player_position, player_velocity, mace_position,
+                 mace_velocity):
+        self.id = id_num #pylint: disable=invalid-name
+        self.position = player_position
+        self.velocity = player_velocity
+        self.mace = Mace(self, mace_position, mace_velocity)
+        self.dead = False
+        self.name = None
 
 class Arena:
     """Represents an arena"""
     def __init__(self):
+        self.current_player = None
         self.players = dict() # id -> Player
+        self.dimensions = None
+        self.target_dimensions = None # TODO: Implement dimension transitions
 
 class Leaderboard:
     """Represents a leaderboard"""
@@ -59,26 +74,70 @@ class Session:
         self._loop = loop
         self._connection = None
         self.mode = mode
-        self.arena = None
+        self.arena = Arena()
 
     def _send_play_packet(self):
         self._connection.send(dict(type="play"))
 
     def _received_packet_handler(self, packet):
         if packet.type == "setup":
-            print("setup", packet.payload.server_version, packet.payload.syncer_value,
-                  packet.payload.setup_value)
+            print("setup", packet.payload.current_player_id)
+            if not packet.payload.game_mode is self.mode:
+                raise ValueError(
+                    "Server-reported mode {} does not match current mode {}".format(
+                        packet.payload.mode.value, self.mode.value
+                    )
+                )
+            self.arena.dimensions = packet.payload.dimensions
+            self.arena.target_dimensions = packet.payload.target_dimensions
+            initial_vectorstruct = (None, None)
+            self.arena.current_player = Player(
+                packet.payload.current_player_id,
+                Vector(coordinates=initial_vectorstruct),
+                Vector(coordinates=initial_vectorstruct),
+                Vector(coordinates=initial_vectorstruct),
+                Vector(coordinates=initial_vectorstruct)
+            )
+            self.arena.players[packet.payload.current_player_id] = self.arena.current_player
         elif packet.type == "killed":
             print("Got killed. Respawning...")
             self._send_play_packet()
         elif packet.type == "kill":
-            print("kill", "-", "stamp", packet.payload.stamp)
+            print("kill", "-", "killed_id", packet.payload.killed_id, "killer_id",
+                  packet.payload.killer_id)
+            del self.arena.players[packet.payload.killed_id]
+            if packet.payload.killed_id == self.arena.current_player.id:
+                self.arena.current_player.name = None
         elif packet.type == "remove":
             print("remove", "-", packet.payload.player_id)
+            del self.arena.players[packet.payload.player_id]
         elif packet.type == "sync":
-            pass
-            #print("sync", "-", "remove_count", packet.payload.remove_count,
-            #      "sync_count", packet.payload.sync_count)
+            print("sync -", end="")
+            for player_id in packet.payload.removal_array:
+                print(" delete", player_id)
+                del self.arena.players[player_id]
+            for sync_struct in packet.payload.sync_array:
+                if sync_struct.is_new_player:
+                    player_obj = Player(
+                        sync_struct.player_id,
+                        Vector(vector_struct=sync_struct.player_state.position),
+                        Vector(vector_struct=sync_struct.player_state.velocity),
+                        Vector(vector_struct=sync_struct.mace_state.position),
+                        Vector(vector_struct=sync_struct.mace_state.velocity)
+                    )
+                    if sync_struct.player_attributes.player_name:
+                        print("New name: " + sync_struct.player_attributes.player_name)
+                        player_obj.name = sync_struct.player_attributes.player_name
+                    self.arena.players[sync_struct.player_id] = player_obj
+                    print(" new", sync_struct.player_id, end="")
+                else:
+                    player = self.arena.players[sync_struct.player_id]
+                    player.position.update(sync_struct.player_state.position)
+                    player.velocity.update(sync_struct.player_state.velocity)
+                    player.mace.position.update(sync_struct.mace_state.position)
+                    player.mace.velocity.update(sync_struct.mace_state.velocity)
+                    print(" update", sync_struct.player_id, end="")
+            print()
         elif packet.type == "club_collision":
             print("club_collision", "-", "first", packet.payload.first_id,
                   "second", packet.payload.second_id)
@@ -87,12 +146,37 @@ class Session:
         elif packet.type == "set_leaderboard":
             print("set_leaderboard")
         elif packet.type == "set_target_dim":
-            print("set_target_dim", "-", packet.payload.target_dim)
+            print("set_target_dim", "-", packet.payload.target_dimensions)
+            self.arena.target_dimensions = packet.payload.target_dimensions
+        # Debugging purposes
+        if len(packet.extraneous) > 0:
+            print("EXTRANEOUS", "-", packet.type, "-", len(packet.extraneous))
+            exit()
+
+    def _is_new_player(self, player_id):
+        print("checking is new player: {}".format(player_id))
+        if player_id == self.arena.current_player.id:
+            print("Current player lacks name?: {}".format(not bool(self.arena.current_player.name)))
+            if not self.arena.current_player.name:
+                self.arena.current_player.name = True
+                return True
+            return False
+        print("Is unknown player?: {}".format(player_id not in self.arena.players))
+        return player_id not in self.arena.players
+
+    def _is_current_player(self, player_id):
+        print("check: {}, known: {}".format(player_id, self.arena.current_player.id))
+        return player_id == self.arena.current_player.id
 
     async def start(self, address, port):
         """Start the session"""
         async with aiohttp.ClientSession(loop=self._loop) as session:
             async with session.ws_connect("ws://" + address + ":" + str(port)) as websocket:
-                self._connection = Connection(websocket, self.mode)
+                self._connection = Connection(
+                    websocket,
+                    self.mode,
+                    self._is_new_player,
+                    self._is_current_player
+                )
                 self._send_play_packet()
                 await self._connection.listen_loop(self._received_packet_handler)
--- autozlap/networking.py
+++ autozlap/networking.py
@@ -40,9 +40,11 @@ physical_state = construct.Struct( #pylint: disable=invalid-name
 class Connection:
     """Represents the stateful networking connection"""
 
-    def __init__(self, websocket, mode):
+    def __init__(self, websocket, mode, new_player_checker, current_player_checker):
         self._websocket = websocket
         self._mode = mode
+        self._is_new_player = new_player_checker
+        self._is_current_player = current_player_checker
         self._inbound_packet = construct.Struct(
             "type" / construct.Enum(
                 construct.Int8ul,
@@ -60,8 +62,8 @@ class Connection:
                 construct.this.type, {
                     "setup": construct.Struct(
                         "server_version" / construct.CString(encoding="utf8"),
-                        construct.Check(lambda ctx: ctx.server_version == CLIENT_VERSION),
-                        "syncer_value" / construct.Int32ul,
+                        construct.Check(construct.this.server_version == CLIENT_VERSION),
+                        "initial_time" / construct.Int32ul,
                         "game_mode" / construct.Mapping(
                             construct.Int8ul, {
                                 0: Mode.ffa,
@@ -69,12 +71,20 @@ class Connection:
                             },
                             dict() # This packet is only received
                         ),
-                        "setup_value" / construct.Int32ul
+                        "current_player_id" / construct.Int32ul,
+                        "dimensions" / vector2d,
+                        "target_dimensions" / vector2d
                     ),
                     "killed": construct.Pass,
-                    "kill": construct.Struct("stamp" / construct.Int32ul),
+                    "kill": construct.Struct(
+                        "timestamp" / construct.Int32ul,
+                        "killed_id" / construct.Int32ul,
+                        "death_position" / vector2d,
+                        "killer_id" / construct.Int32ul,
+                        "point_orb_count" / construct.Int32ul
+                    ),
                     "remove": construct.Struct(
-                        "enqueue_param" / construct.Int32ul, # TODO: Figure out purpose
+                        "timestamp" / construct.Int32ul,
                         "player_id" / construct.Int32ul
                     ),
                     "sync": construct.Struct(
@@ -89,6 +99,25 @@ class Connection:
                             construct.this.sync_count,
                             construct.Struct(
                                 "player_id" / construct.Int32ul,
+                                "is_new_player" / construct.Computed(
+                                    lambda ctx: self._is_new_player(ctx.player_id)
+                                ),
+                                "player_attributes" / construct.If(
+                                    construct.this.is_new_player,
+                                    construct.Embedded(
+                                        construct.Struct(
+                                            "player_name" / construct.IfThenElse(
+                                                lambda ctx: not self._is_current_player(
+                                                    ctx._.player_id
+                                                ),
+                                                construct.CString(encoding="utf8"),
+                                                construct.Pass
+                                            ),
+                                            "shield" / construct.Float32l,
+                                            "team_or_skin" / construct.Int8ul
+                                        )
+                                    )
+                                ),
                                 "player_state" / physical_state,
                                 "mace_state" / physical_state,
                                 "mace_radius" / construct.Float32l
@@ -96,7 +125,7 @@ class Connection:
                         )
                     ),
                     "club_collision": construct.Struct(
-                        "enqueue_param" / construct.Int32ul, # TODO: Figure out purpose
+                        "timestamp" / construct.Int32ul,
                         "p" / vector2d, # TODO: Figure out purpose
                         "i" / construct.Float32l, # TODO: Figure out purpose
                         "first_id" / construct.Int32ul,
@@ -105,7 +134,7 @@ class Connection:
                         "second_state" / physical_state
                     ),
                     "wall_collision": construct.Struct(
-                        "enqueue_param" / construct.Int32ul, # TODO: Figure out purpose
+                        "timestamp" / construct.Int32ul,
                         "p" / vector2d, # TODO: Figure out purpose
                         "i" / construct.Float32l, # TODO: Figure out purpose
                         "player_id" / construct.Int32ul,
@@ -115,6 +144,7 @@ class Connection:
                     "set_leaderboard": construct.Struct(
                         "player_count" / construct.Int32ul,
                         "total" / construct.Int32ul, # TODO: Figure out purpose,
+                        construct.Check(lambda ctx: self._mode in Mode),
                         construct.IfThenElse(
                             lambda ctx: self._mode == Mode.ffa,
                             construct.Struct( # FFA
@@ -142,9 +172,10 @@ class Connection:
                             )
                         )
                     ),
-                    "set_target_dim": construct.Struct("target_dim" / vector2d)
+                    "set_target_dim": construct.Struct("target_dimensions" / vector2d)
                 }
-            )
+            ),
+            "extraneous" / construct.GreedyBytes # For debugging
         )
         self._outbound_packet = construct.Struct(
             "type" / construct.Enum(
@@ -172,7 +203,11 @@ class Connection:
         """Async listening loop"""
         async for msg in self._websocket:
             if msg.type == aiohttp.WSMsgType.BINARY:
-                parsed_callback(self._inbound_packet.parse(msg.data))
+                parsed_packet = self._inbound_packet.parse(msg.data)
+                if parsed_packet.extraneous: # For debugging
+                    print("Entire packet with extraneous: " + str(msg.data))
+                    print("Packet: " + str(parsed_packet))
+                parsed_callback(parsed_packet)
             else:
                 raise ValueError("Unexpected data type: " + msg.type.name)
 
